b2c6433d38d0099c248add384a34f95f1119abca
John Paul Manchego Medina <jmanchem@everis.com>
2021-08-18 09:35:53 -0500

    add bill

---
+++ src/main/java/com/example/mspasives/repositories/BillRepository.java
@@ -0,0 +1,9 @@
+package com.example.mspasives.repositories;
+
+import com.example.mspasives.models.entities.Bill;
+import reactor.core.publisher.Mono;
+
+public interface BillRepository extends IRepository<Bill, String>{
+    Mono<Bill> findByAccountNumber(String accountNumber);
+    Mono<Bill> findByAcquisitionCardNumber(String cardNumber);
+}
