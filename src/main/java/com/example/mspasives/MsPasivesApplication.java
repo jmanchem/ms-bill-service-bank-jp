b81aff447e5d161d7dbee5126eb2cc400bcc84fd
John Paul Manchego Medina <jmanchem@everis.com>
2021-08-25 23:57:59 -0500

    add docker file

--- src/main/java/com/example/mspasives/MsPasivesApplication.java
+++ src/main/java/com/example/mspasives/MsPasivesApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 public class MsPasivesApplication {
 
     public static void main(String[] args) {
